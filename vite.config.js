e53d6391d91f47f036aa6757307ff98e34021fc1
benmarr <benmarr2@gmail.com>
2022-02-16 19:58:56 +0000

    Trigger rebuild

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vitePluginString from 'vite-plugin-string'
 export default {
     plugins: [
         vitePluginString()
-    ]
+    ],
+    build: {
+        chunkSizeWarningLimit: 2048 // 2kb
+    }
 }
